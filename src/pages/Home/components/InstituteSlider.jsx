f749c5587132845310e8a548a4c803b2453b59a7
developerashikur1 <masik4506@gmail.com>
2023-02-25 00:15:17 +0600

    auto slid done.

--- src/pages/Home/components/InstituteSlider.jsx
+++ src/pages/Home/components/InstituteSlider.jsx
@@ -1,7 +1,7 @@
 import { Box, Typography } from '@mui/material';
 import React from 'react';
 // import required modules
-import { FreeMode, Navigation } from "swiper";
+import { Autoplay, FreeMode, Navigation } from "swiper";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/free-mode";
@@ -12,7 +12,7 @@ import instituteImg1 from '../../../assets/images/company-slider-images/Asset-1.
 import instituteImg2 from '../../../assets/images/company-slider-images/Asset-2.png';
 import instituteImg3 from '../../../assets/images/company-slider-images/Asset-3.png';
 import instituteImg4 from '../../../assets/images/company-slider-images/Asset-4.png';
-import { default as instituteImg10, default as instituteImg5 } from '../../../assets/images/company-slider-images/Asset-5.png';
+import { default as instituteImg5 } from '../../../assets/images/company-slider-images/Asset-5.png';
 import instituteImg6 from '../../../assets/images/company-slider-images/Asset-6.png';
 import instituteImg7 from '../../../assets/images/company-slider-images/Asset-7.png';
 import instituteImg8 from '../../../assets/images/company-slider-images/Asset-8.png';
@@ -33,7 +33,15 @@ const instituteContents = [
     {id : 7, img : instituteImg7},
     {id : 8, img : instituteImg8},
     {id : 9, img : instituteImg9},
-    {id : 10, img : instituteImg10},
+    {id : 10, img : instituteImg1},
+    {id : 11, img : instituteImg2},
+    {id : 12, img : instituteImg3},
+    {id : 13, img : instituteImg4},
+    {id : 14, img : instituteImg5},
+    {id : 15, img : instituteImg6},
+    {id : 16, img : instituteImg7},
+    {id : 17, img : instituteImg8},
+    {id : 18, img : instituteImg9},
 ]
 
 const InstituteSlider = () => {
@@ -53,7 +61,11 @@ const InstituteSlider = () => {
                     navigation={{
                     clickable: true,
                     }}
-                    modules={[FreeMode, Navigation]}
+                    autoplay={{
+                    delay: 1500,
+                    disableOnInteraction: false,
+                    }}
+                    modules={[FreeMode, Navigation, Autoplay]}
                     className="institute-swiper-slider"
                 >
                     {instituteContents.map((institute) => 
